34ce35f497a212ee63bb1caa5f41471119fc4f71
The4thOrphan <s01325429@acad.tri-c.edu>
2025-03-21 17:59:47 +0000

    Create Lab 1

---
+++ Lab1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Lab1;
+
+using System;
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Step 1
+Console.WriteLine("Mason Pierce");
+Console.WriteLine("IT-1050");
+     // Step 2
+      /* My name is Mason Pierce
+      My class is IT-1050 */
+      // Step 3
+      int theBurgerlimit = 2;
+     Console.WriteLine("I can eat "+ theBurgerlimit +" burgers before my tummy hurt.");
+      string codingSoftware = "Python";
+      Console.WriteLine("My favorite programming language is "+ codingSoftware +" it is cool.");
+      double codes = 4.0;
+      Console.WriteLine("I have written "+ codes +" different codes before this, I think. Not actually sure on the exact number.");
+      bool experience = true;    
+      Console.WriteLine("Yes it is "+ experience +" that I do have coding experience. Not a lot though.");
+      // Step 4
+      const string mySchool = "Fairview Park Highschool and Tri-C";
+      Console.WriteLine("I go to both "+ mySchool +" though I only go to Tri-C fro classes.");
+
+    }
+  }
