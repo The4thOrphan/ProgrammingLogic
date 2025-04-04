f06865b04ca33feefba951402da341c4ffd1f945
The4thOrphan <s01325429@acad.tri-c.edu>
2025-04-04 16:32:30 +0000

    Create Lab 3

--- Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
+++ Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34ce35f497a212ee63bb1caa5f41471119fc4f71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+595c8996a2b2b60ce0bc59640352eb117538471c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lab3/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Transactions;
+
+{
+//Part 1
+int a = 10;
+int b = 5;
+int c = 15;
+Console.WriteLine(a > b);
+Console.WriteLine(a < c);
+Console.WriteLine(a > b && a > c);
+//Part 2
+bool isRaining = true;
+bool haveUmbrella = false;
+if (isRaining && !haveUmbrella)
+{
+    Console.WriteLine("Take an Umbrella.");
+}
+else
+{
+    Console.WriteLine("You're good to go.");
+}
+//Part 3
+Console.WriteLine("Enter your age:");
+int age = Convert.ToInt32(Console.ReadLine());
+if (age < 5)
+{
+    Console.WriteLine("Tickets are free");
+}
+else if (age > 5 && age < 12)
+{
+    Console.WriteLine("Child ticket: 5 dollars");
+}
+else if (age > 13 && age < 64)
+{
+    Console.WriteLine("Standard ticket: 10 dollars");
+}
+else if (age >= 65)
+{
+    Console.WriteLine("Senoir ticket: 6 dollars");
+}
+//Part 4 
+Console.WriteLine("Enter a number: ");
+int dayofWeek = Convert.ToInt32(Console.ReadLine());
+switch (dayofWeek) 
+{
+  case 1:
+    Console.WriteLine("Monday");
+    break;
+  case 2:
+    Console.WriteLine("Tuesday");
+    break;
+  case 3:
+    Console.WriteLine("Wednesday");
+    break;
+  case 4:
+    Console.WriteLine("Thursday");
+    break;
+  case 5:
+    Console.WriteLine("Friday");
+    break;
+  case 6:
+    Console.WriteLine("Saturday");
+    break;
+  case 7:
+    Console.WriteLine("Sunday");
+    break;
+  default:
+   Console.WriteLine("Invalid day");
+break;
+}
+}
